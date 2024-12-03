4b2f9cb103966d6f813cf6ba62fa8421ba36c518
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-03 07:50:56 +0100

    add day-03-1 (2024)

---
+++ 2024/day-03/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"advent-of-code-go/utils"
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	part, realData := utils.GetRunConfig(1, true)
+
+	data := utils.ReadFileAsByteArray(utils.GetFileName(2024, 3, realData))
+
+	var result int
+	if part == 1 {
+		result = part1(data)
+	} else {
+		result = part2(data)
+	}
+
+	fmt.Println(result)
+}
+
+func part1(data [][]byte) int {
+	result := 0
+	r := regexp.MustCompile("mul\\(\\d{1,3},\\d{1,3}\\)")
+
+	for _, line := range data {
+
+		finds := r.FindAll(line, -1)
+
+		for _, find := range finds {
+			exp := string(find)
+			var a, b int
+			fmt.Sscanf(exp, "mul(%d,%d)", &a, &b)
+			result += a * b
+		}
+	}
+
+	return result
+}
+
+func part2(data [][]byte) int {
+	return 0
+}
