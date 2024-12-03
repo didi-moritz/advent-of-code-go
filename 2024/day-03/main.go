f6d3568d0e17e0865558b17fdd51a6445066d2e1
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-03 07:56:47 +0100

    add day-03-2 (2024)

--- 2024/day-03/main.go
+++ 2024/day-03/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	part, realData := utils.GetRunConfig(1, true)
+	part, realData := utils.GetRunConfig(2, true)
 
 	data := utils.ReadFileAsByteArray(utils.GetFileName(2024, 3, realData))
 
@@ -41,5 +41,34 @@ func part1(data [][]byte) int {
 }
 
 func part2(data [][]byte) int {
-	return 0
+	result := 0
+	r := regexp.MustCompile("(mul\\(\\d{1,3},\\d{1,3}\\)|do(n't)?\\(\\))")
+	do := true
+
+	for _, line := range data {
+
+		finds := r.FindAll(line, -1)
+
+		for _, find := range finds {
+			exp := string(find)
+
+			if exp == "do()" {
+				do = true
+				continue
+			} else if exp == "don't()" {
+				do = false
+				continue
+			}
+
+			if !do {
+				continue
+			}
+
+			var a, b int
+			fmt.Sscanf(exp, "mul(%d,%d)", &a, &b)
+			result += a * b
+		}
+	}
+
+	return result
 }
