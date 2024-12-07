4d6a3750fafd90547cf3440de7d73e277aec5a50
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-07 07:30:40 +0100

    add day-07-2 (2024)

--- 2024/day-07/main.go
+++ 2024/day-07/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"advent-of-code-go/utils"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	part, realData := utils.GetRunConfig(1, false)
+	part, realData := utils.GetRunConfig(2, false)
 
 	data := utils.ReadFileAsStringArray(utils.GetFileName(2024, 7, realData))
 
@@ -67,6 +68,52 @@ func checkRecursively(check int, numbers []int, pos int, sum int) int {
 }
 
 func part2(data []string) int {
+	result := 0
+	for _, line := range data {
+		parts := strings.Split(line, ": ")
 
-	return 0
+		check := int(utils.ParseInt(parts[0]))
+
+		words := strings.Split(parts[1], " ")
+		numbers := make([]int, len(words))
+		for i, word := range words {
+			numbers[i] = int(utils.ParseInt(word))
+		}
+
+		sum := checkRecursively2(check, numbers, 1, numbers[0])
+
+		if sum == check {
+			result += check
+		}
+	}
+
+	return result
+}
+
+func checkRecursively2(check int, numbers []int, pos int, sum int) int {
+	if sum > check {
+		return -1
+	}
+
+	if pos == len(numbers) {
+		return sum
+	}
+
+	sumAdd := checkRecursively2(check, numbers, pos+1, sum+numbers[pos])
+	if sumAdd == check {
+		return check
+	}
+
+	sumMultiplication := checkRecursively2(check, numbers, pos+1, sum*numbers[pos])
+	if sumMultiplication == check {
+		return check
+	}
+
+	concat := int(utils.ParseInt(strconv.Itoa(sum) + strconv.Itoa(numbers[pos])))
+	sumConcat := checkRecursively2(check, numbers, pos+1, concat)
+	if sumConcat == check {
+		return check
+	}
+
+	return -1
 }
