71fe9c9c70e27d63a09dcc3ae1b7c33a0e67ac4c
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-07 07:20:42 +0100

    add day-07-1 (2024)

---
+++ 2024/day-07/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"advent-of-code-go/utils"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	part, realData := utils.GetRunConfig(1, false)
+
+	data := utils.ReadFileAsStringArray(utils.GetFileName(2024, 7, realData))
+
+	var result int
+	if part == 1 {
+		result = part1(data)
+	} else {
+		result = part2(data)
+	}
+
+	fmt.Println(result)
+}
+
+func part1(data []string) int {
+	result := 0
+	for _, line := range data {
+		parts := strings.Split(line, ": ")
+
+		check := int(utils.ParseInt(parts[0]))
+
+		words := strings.Split(parts[1], " ")
+		numbers := make([]int, len(words))
+		for i, word := range words {
+			numbers[i] = int(utils.ParseInt(word))
+		}
+
+		sum := checkRecursively(check, numbers, 1, numbers[0])
+
+		if sum == check {
+			result += check
+		}
+	}
+
+	return result
+}
+
+func checkRecursively(check int, numbers []int, pos int, sum int) int {
+	if sum > check {
+		return -1
+	}
+
+	if pos == len(numbers) {
+		return sum
+	}
+
+	sumAdd := checkRecursively(check, numbers, pos+1, sum+numbers[pos])
+	if sumAdd == check {
+		return check
+	}
+
+	sumMultiplication := checkRecursively(check, numbers, pos+1, sum*numbers[pos])
+	if sumMultiplication == check {
+		return check
+	}
+
+	return -1
+}
+
+func part2(data []string) int {
+
+	return 0
+}
