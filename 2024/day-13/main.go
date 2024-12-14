96a58ec7645ea79ed3eab8285fb3a571f1c3cb9b
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-14 14:53:54 +0100

    add day-13-2 (2024)

--- 2024/day-13/main.go
+++ 2024/day-13/main.go
@@ -18,6 +18,17 @@ func main() {
 
 	data := utils.ReadFileAsStringArray(utils.GetFileName(2024, 13, realData))
 
+	var result int
+	if part == 1 {
+		result = part1(data)
+	} else {
+		result = part2(data)
+	}
+
+	fmt.Println(result)
+}
+
+func part1(data []string) int {
 	var machines []machine
 
 	for i := range (len(data) + 1) / 4 {
@@ -33,17 +44,6 @@ func main() {
 		machines = append(machines, m)
 	}
 
-	var result int
-	if part == 1 {
-		result = part1(machines)
-	} else {
-		result = part2(data)
-	}
-
-	fmt.Println(result)
-}
-
-func part1(machines []machine) int {
 	result := 0
 	for _, m := range machines {
 		result += calcTokens(m)
@@ -52,13 +52,12 @@ func part1(machines []machine) int {
 }
 
 func calcTokens(m machine) int {
-	minimum := 0
 	for ia := range 101 {
 		xa := ia * m.a.x
 		ya := ia * m.a.y
 
 		if xa > m.prize.x || ya > m.prize.y {
-			return minimum
+			return 0
 		}
 
 		for ib := range 101 {
@@ -66,10 +65,9 @@ func calcTokens(m machine) int {
 			yb := ib * m.b.y
 
 			if (xa+xb) == m.prize.x && (ya+yb) == m.prize.y {
+				fmt.Println(ia, ib)
 				tokens := ia*3 + ib
-				if tokens < minimum || minimum == 0 {
-					minimum = tokens
-				}
+				return tokens
 			}
 
 			if (xa+xb) >= m.prize.x || (ya+yb) >= m.prize.y {
@@ -79,9 +77,78 @@ func calcTokens(m machine) int {
 		}
 	}
 
-	return minimum
+	return 0
+}
+
+type button struct {
+	v      v
+	tokens int
+}
+
+type machine2 struct {
+	a, b  button
+	prize v
 }
 
 func part2(data []string) int {
+	var machines []machine2
+
+	for i := range (len(data) + 1) / 4 {
+		m := machine2{}
+		var x, y int
+		fmt.Sscanf(data[i*4], "Button A: X+%d, Y+%d", &x, &y)
+		m.a = button{v{x, y}, 3}
+		fmt.Sscanf(data[i*4+1], "Button B: X+%d, Y+%d", &x, &y)
+		m.b = button{v{x, y}, 1}
+		fmt.Sscanf(data[i*4+2], "Prize: X=%d, Y=%d", &x, &y)
+		m.prize = v{x, y}
+
+		machines = append(machines, m)
+	}
+
+	result := 0
+	for _, m := range machines {
+		m.prize.x += 10000000000000
+		m.prize.y += 10000000000000
+		tokens := calcTokens2(m)
+		result += tokens
+	}
+
+	return result
+}
+
+func calcTokens2(m machine2) int {
+	a := float64(m.a.v.x)
+	b := float64(m.b.v.x)
+	c := float64(m.prize.x)
+
+	d := float64(m.a.v.y)
+	e := float64(m.b.v.y)
+	f := float64(m.prize.y)
+
+	ib := int((f - (c * d / a)) / (e - (b * d / a)))
+
+	for ibc := ib - 10; ibc < ib+10; ibc++ {
+
+		x := ib * m.b.v.x
+		y := ib * m.b.v.y
+
+		rx := m.prize.x - x
+		ry := m.prize.y - y
+
+		var ia int
+		if rx < ry {
+			ia = rx / m.a.v.x
+		} else {
+			ia = ry / m.a.v.y
+		}
+
+		for iac := ia - 10; iac < ia+10; iac++ {
+			if iac*m.a.v.x+ibc*m.b.v.x == m.prize.x && iac*m.a.v.y+ibc*m.b.v.y == m.prize.y {
+				return iac*m.a.tokens + ibc*m.b.tokens
+			}
+		}
+	}
+
 	return 0
 }
