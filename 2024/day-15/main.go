8b79a6706d8086d8a66887c55495544ee56085a3
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-15 09:03:45 +0100

    add day-15-2 (2024)

--- 2024/day-15/main.go
+++ 2024/day-15/main.go
@@ -11,7 +11,7 @@ type v struct {
 }
 
 func main() {
-	part, realData := utils.GetRunConfig(1, false)
+	part, realData := utils.GetRunConfig(2, false)
 
 	data := utils.ReadFileAsByteArray(utils.GetFileName(2024, 15, realData))
 
@@ -51,7 +51,7 @@ func main() {
 	if part == 1 {
 		result = part1(m, robot, commands, !realData)
 	} else {
-		result = part2(data)
+		result = part2(m, robot, commands, !realData)
 	}
 
 	fmt.Println(result)
@@ -81,7 +81,7 @@ func part1(m [][]byte, robot v, commands []v, showMap bool) int {
 
 		if showMap {
 			printMap(m, robot)
-			time.Sleep(10 * time.Millisecond)
+			time.Sleep(100 * time.Millisecond)
 		}
 	}
 
@@ -112,6 +112,145 @@ func printMap(m [][]byte, robot v) {
 	}
 }
 
-func part2(data [][]byte) int {
-	return 0
+func part2(m [][]byte, robot v, commands []v, showMap bool) int {
+	for y, line := range m {
+		var newLine []byte
+		for _, c := range line {
+			if c == 'O' {
+				newLine = append(newLine, '[')
+				newLine = append(newLine, ']')
+			} else {
+				newLine = append(newLine, c)
+				newLine = append(newLine, c)
+			}
+		}
+		m[y] = newLine
+	}
+
+	robot.x = robot.x * 2
+
+	printMap(m, robot)
+
+	for _, c := range commands {
+		x := robot.x + c.x
+		y := robot.y + c.y
+
+		newPos := v{x, y}
+
+		stillOk := !isBlocked(m, newPos)
+
+		if stillOk {
+			if isBlock(m, newPos) {
+				stillOk = moveBoxes(m, newPos, c, false)
+				if stillOk {
+					moveBoxes(m, newPos, c, true)
+				}
+			}
+		}
+
+		if stillOk {
+			robot.x += c.x
+			robot.y += c.y
+		}
+
+		if showMap {
+			printMap(m, robot)
+			fmt.Println(c)
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+
+	printMap(m, robot)
+
+	result := 0
+	for y, line := range m {
+		for x, c := range line {
+			if c == '[' {
+				result += y*100 + x
+			}
+		}
+	}
+
+	return result
+}
+
+func moveBoxes(m [][]byte, from v, move v, doPush bool) bool {
+	if isFree(m, from) {
+		return true
+	}
+
+	verticalMove := move.x == 0
+
+	part1 := from
+	part2 := findOtherPartOfBlock(m, part1)
+
+	to1 := v{part1.x + move.x, part1.y + move.y}
+	to2 := v{part2.x + move.x, part2.y + move.y}
+
+	if isBlocked(m, to1) || isBlocked(m, to2) {
+		return false
+	}
+
+	if verticalMove {
+		if isBlock(m, to1) {
+			if !moveBoxes(m, to1, move, doPush) {
+				return false
+			}
+		}
+
+		if isBlock(m, to2) {
+			if !moveBoxes(m, to2, move, doPush) {
+				return false
+			}
+		}
+	} else {
+		relevantTo := to1
+		if to1 == part2 {
+			relevantTo = to2
+		}
+
+		if isBlocked(m, relevantTo) {
+			return false
+		}
+
+		if isBlock(m, relevantTo) {
+			if !moveBoxes(m, relevantTo, move, doPush) {
+				return false
+			}
+		}
+	}
+
+	if doPush {
+		moveBox(m, part1, part2, move)
+	}
+	return true
+}
+
+func moveBox(m [][]byte, part1 v, part2 v, move v) {
+	c1 := m[part1.y][part1.x]
+	c2 := m[part2.y][part2.x]
+	m[part1.y][part1.x] = '.'
+	m[part2.y][part2.x] = '.'
+	m[part1.y+move.y][part1.x+move.x] = c1
+	m[part2.y+move.y][part2.x+move.x] = c2
+}
+
+func isBlock(m [][]byte, p v) bool {
+	return m[p.y][p.x] == '[' || m[p.y][p.x] == ']'
+}
+
+func findOtherPartOfBlock(m [][]byte, p v) v {
+	if m[p.y][p.x] == '[' {
+		return v{p.x + 1, p.y}
+	} else {
+		return v{p.x - 1, p.y}
+	}
+}
+
+func isBlocked(m [][]byte, p v) bool {
+	return m[p.y][p.x] == '#'
+}
+
+func isFree(m [][]byte, p v) bool {
+	return m[p.y][p.x] == '.'
 }
