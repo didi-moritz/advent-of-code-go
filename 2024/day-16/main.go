c7c5ee45544635bf7fa78ab0e82def7e34197bde
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-16 20:47:01 +0100

    add day-16-2 (2024)

--- 2024/day-16/main.go
+++ 2024/day-16/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	part, realData := utils.GetRunConfig(1, false)
+	part, realData := utils.GetRunConfig(2, false)
 
 	data := utils.ReadFileAsByteArray(utils.GetFileName(2024, 16, realData))
 
@@ -49,10 +49,10 @@ func part1(data [][]byte) int {
 		}
 	}
 
-	return move(data, end, start.x, start.y, 0, true, 0, found, make([]v, 0))
+	return move(data, end, start.x, start.y, 0, 0, found)
 }
 
-func move(data [][]byte, end v, x int, y int, dir int, canRotate bool, score int, found [][]int, visited []v) int {
+func move(data [][]byte, end v, x int, y int, dir int, score int, found [][]int) int {
 	minimum := math.MaxInt
 
 	p := v{x, y}
@@ -71,25 +71,13 @@ func move(data [][]byte, end v, x int, y int, dir int, canRotate bool, score int
 		return score
 	}
 
-	if slices.Contains(visited, p) {
-		return minimum
-	}
-
-	visited = append(visited, p)
-
 	found[y][x] = score
 
 	for i, newDir := range []int{dir, dir - 1, dir + 1} {
 		addScore := 1
 
-		//if !canRotate && i > 1 {
-		//	continue
-		//}
-
-		newCanRotate := true
 		if i > 0 {
 			addScore = 1001
-			newCanRotate = false
 		}
 
 		if newDir < 0 {
@@ -98,7 +86,7 @@ func move(data [][]byte, end v, x int, y int, dir int, canRotate bool, score int
 
 		newDir = newDir % 4
 
-		newScore := move(data, end, x+moves[newDir].x, y+moves[newDir].y, newDir, newCanRotate, score+addScore, found, visited)
+		newScore := move(data, end, x+moves[newDir].x, y+moves[newDir].y, newDir, score+addScore, found)
 		if newScore < minimum {
 			minimum = newScore
 		}
@@ -108,5 +96,107 @@ func move(data [][]byte, end v, x int, y int, dir int, canRotate bool, score int
 }
 
 func part2(data [][]byte) int {
-	return 0
+	var start, end v
+
+	found := make([][][]int, len(data))
+
+	for y, line := range data {
+		found[y] = make([][]int, len(line))
+		for x, c := range line {
+			found[y][x] = make([]int, 4)
+			for d := range 4 {
+				found[y][x][d] = math.MaxInt
+			}
+			if c == 'S' {
+				start = v{x, y}
+				data[y][x] = '.'
+			} else if c == 'E' {
+				end = v{x, y}
+				data[y][x] = '.'
+			}
+		}
+	}
+
+	score, visited := move2(data, end, start.x, start.y, 0, 0, found, make([]v, 0))
+	fmt.Println(score)
+	printMap(data, visited)
+	return len(visited)
+}
+
+func move2(data [][]byte, end v, x int, y int, dir int, score int, found [][][]int, visited []v) (int, []v) {
+	minimum := math.MaxInt
+
+	p := v{x, y}
+
+	if data[y][x] == '#' {
+		return minimum, nil
+	}
+
+	if found[y][x][dir] > 0 {
+		if score > found[y][x][dir] {
+			return minimum, visited
+		}
+	}
+
+	if p == end {
+		newVisited := make([]v, len(visited))
+		copy(newVisited, visited)
+		return score, append(newVisited, p)
+	}
+
+	if slices.Contains(visited, p) {
+		return minimum, nil
+	}
+
+	newVisited := make([]v, len(visited))
+	copy(newVisited, visited)
+	visited = append(newVisited, p)
+
+	found[y][x][dir] = score
+
+	var returnVisited []v
+
+	for i, newDir := range []int{dir, dir - 1, dir + 1} {
+		addScore := 1
+
+		if i > 0 {
+			addScore = 1001
+		}
+
+		if newDir < 0 {
+			newDir += 4
+		}
+
+		newDir = newDir % 4
+
+		newScore, newVisited := move2(data, end, x+moves[newDir].x, y+moves[newDir].y, newDir, score+addScore, found, visited)
+
+		if newScore < minimum && newScore < math.MaxInt {
+			minimum = newScore
+			returnVisited = newVisited
+		} else if newScore <= minimum && newScore < math.MaxInt {
+			minimum = newScore
+			for _, v := range newVisited {
+				if !slices.Contains(returnVisited, v) {
+					returnVisited = append(returnVisited, v)
+				}
+			}
+		}
+	}
+
+	return minimum, returnVisited
+}
+
+func printMap(data [][]byte, visited []v) {
+	for y, line := range data {
+		for x, c := range line {
+			p := v{x, y}
+			if slices.Contains(visited, p) {
+				fmt.Print("O")
+			} else {
+				fmt.Print(string(c))
+			}
+		}
+		fmt.Println()
+	}
 }
