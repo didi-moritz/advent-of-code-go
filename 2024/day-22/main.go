49ccd3c6af1a347430c4d33fb135f424465d81e9
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-22 17:32:54 +0100

    add day-22-2 (2024)

--- 2024/day-22/main.go
+++ 2024/day-22/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	part, realData := utils.GetRunConfig(1, false)
+	part, realData := utils.GetRunConfig(2, false)
 
 	data := utils.ReadFileAsStringArray(utils.GetFileName(2024, 22, realData))
 
@@ -49,6 +49,74 @@ func mix(a int, b int) int {
 func prune(a int) int {
 	return a % 16777216
 }
+
+type entry struct {
+	score, diff int
+}
+
 func part2(data []string) int {
-	return 0
+	allEntries := make([][]entry, len(data))
+
+	result := 0
+	for i, line := range data {
+		allEntries[i] = make([]entry, 2000-1)
+		previousNumber, _ := strconv.Atoi(line)
+		for j := range 2000 - 1 {
+			nextNumber := calcNextPseudoNumber(previousNumber)
+
+			nextDigit := nextNumber % 10
+			previousDigit := previousNumber % 10
+
+			allEntries[i][j] = entry{nextDigit, nextDigit - previousDigit}
+
+			previousNumber = nextNumber
+		}
+	}
+
+	for i1 := -9; i1 < 10; i1++ {
+		for i2 := -9; i2 < 10; i2++ {
+			for i3 := -9; i3 < 10; i3++ {
+				for i4 := -9; i4 < 10; i4++ {
+					sequence := []int{i1, i2, i3, i4}
+					newResult := calcBananas(sequence, allEntries)
+					if newResult > result {
+						result = newResult
+					}
+					fmt.Println(sequence, newResult)
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func calcBananas(sequence []int, allEntries [][]entry) int {
+	allBananas := 0
+	for _, entries := range allEntries {
+		found, bananas := findSequence(sequence, entries)
+		if found {
+			allBananas += bananas
+		}
+	}
+
+	return allBananas
+}
+
+func findSequence(sequence []int, entries []entry) (bool, int) {
+	for i := 0; i < len(entries)-3; i++ {
+		check := true
+		for j, s := range sequence {
+			if entries[i+j].diff != s {
+				check = false
+				break
+			}
+		}
+
+		if check {
+			return true, entries[i+len(sequence)-1].score
+		}
+	}
+
+	return false, 0
 }
