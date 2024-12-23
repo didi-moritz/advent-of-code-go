0810dc6d8cf2fecd3e816f7e4f014de69e116105
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-23 12:34:13 +0100

    add day-23-2 (2024)

--- 2024/day-23/main.go
+++ 2024/day-23/main.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"slices"
 	"sort"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	part, realData := utils.GetRunConfig(1, false)
+	part, realData := utils.GetRunConfig(2, false)
 
 	data := utils.ReadFileAsByteArray(utils.GetFileName(2024, 23, realData))
 
-	var result int
+	var result string
 	if part == 1 {
 		result = part1(data)
 	} else {
@@ -25,9 +26,7 @@ func main() {
 
 const T = 't'
 
-func part1(data [][]byte) int {
-	map1 := make(map[string][]string)
-	map2 := make(map[string][]string)
+func part1(data [][]byte) string {
 	fullMap := make(map[string][]string)
 
 	var ts []string
@@ -35,9 +34,6 @@ func part1(data [][]byte) int {
 	for _, line := range data {
 		n1 := string(line[0:2])
 		n2 := string(line[3:5])
-
-		map1[n1] = append(map1[n1], n2)
-		map2[n2] = append(map1[n2], n1)
 
 		fullMap[n1] = append(fullMap[n1], n2)
 		fullMap[n2] = append(fullMap[n2], n1)
@@ -72,14 +68,131 @@ func part1(data [][]byte) int {
 		}
 	}
 
-	return len(foundNetworks)
+	return strconv.Itoa(len(foundNetworks))
 }
 
 func buildNetworkName(nodes ...string) string {
 	sort.Strings(nodes)
-	return strings.Join(nodes, "")
+	return strings.Join(nodes, ",")
 }
 
-func part2(data [][]byte) int {
-	return 0
+func part2(data [][]byte) string {
+	fullMap := make(map[string][]string)
+
+	for _, line := range data {
+		n1 := string(line[0:2])
+		n2 := string(line[3:5])
+
+		fullMap[n1] = append(fullMap[n1], n2)
+		fullMap[n2] = append(fullMap[n2], n1)
+	}
+
+	// sort map
+	for _, nodes := range fullMap {
+		sort.Strings(nodes)
+	}
+
+	biggestNetwork := ""
+	biggestNetworkSize := 1
+
+	var checkedNetworks []string
+
+	for node, otherNodes := range fullMap {
+		if len(otherNodes)+1 <= biggestNetworkSize {
+			continue
+		}
+
+		scores := make(map[string][]string)
+
+		for _, otherNode := range otherNodes {
+			for _, otherNode2 := range otherNodes {
+				if otherNode == otherNode2 {
+					continue
+				}
+
+				if slices.Contains(fullMap[otherNode], otherNode2) {
+					scores[otherNode] = append(scores[otherNode], otherNode2)
+				}
+			}
+		}
+
+		for i := biggestNetworkSize + 1; i < len(otherNodes)+2; i++ {
+
+			if i <= biggestNetworkSize {
+				continue
+			}
+
+			networks := getPermutations(otherNodes, i-1)
+
+			for _, network := range networks {
+				if (len(network) + 1) <= biggestNetworkSize {
+					continue
+				}
+
+				check := true
+
+				fullNetwork := make([]string, len(network))
+				copy(fullNetwork, network)
+				fullNetwork = append(fullNetwork, node)
+				networkName := buildNetworkName(fullNetwork...)
+				if slices.Contains(checkedNetworks, networkName) {
+					continue
+				}
+				checkedNetworks = append(checkedNetworks, networkName)
+
+				for _, checkNode := range network {
+					if !check {
+						break
+					}
+
+					checkOtherNodes := fullMap[checkNode]
+					for _, checkOtherNode := range network {
+						if checkOtherNode == checkNode {
+							continue
+						}
+
+						if !slices.Contains(checkOtherNodes, checkOtherNode) {
+							check = false
+							break
+						}
+					}
+				}
+
+				if check {
+					if len(fullNetwork) > biggestNetworkSize {
+						biggestNetworkSize = len(fullNetwork)
+						biggestNetwork = networkName
+						fmt.Println(networkName)
+					}
+				}
+			}
+		}
+	}
+
+	return biggestNetwork
+}
+
+func getPermutations(otherNodes []string, size int) [][]string {
+	var result [][]string
+	getNextPermutation(otherNodes, make([]string, 0), size, &result)
+	return result
+}
+
+func getNextPermutation(remainingOtherNodes []string, selectedNodes []string, size int, result *[][]string) {
+	if len(remainingOtherNodes) < 1 {
+		return
+	}
+
+	newSelectedNodes := make([]string, len(selectedNodes))
+	copy(newSelectedNodes, selectedNodes)
+	newSelectedNodes = append(newSelectedNodes, remainingOtherNodes[0])
+
+	if len(newSelectedNodes) == size {
+		*result = append(*result, newSelectedNodes)
+	}
+
+	if len(remainingOtherNodes) > 0 {
+		getNextPermutation(remainingOtherNodes[1:], newSelectedNodes, size, result)
+		getNextPermutation(remainingOtherNodes[1:], selectedNodes, size, result)
+	}
 }
