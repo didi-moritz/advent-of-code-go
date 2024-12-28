574585499a7e38215bf6849755b5e64cb1391390
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-28 12:55:37 +0100

    add day-9-2 (2020)

--- 2020/day-09/main.go
+++ 2020/day-09/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"advent-of-code-go/utils"
 	"fmt"
+	"math"
 	"strconv"
 )
 
 func main() {
-	part, realData := utils.GetRunConfig(1, false)
+	part, realData := utils.GetRunConfig(2, false)
 
 	data := utils.ReadFileAsStringArray(utils.GetFileName(2020, 9, realData))
 
@@ -15,7 +16,7 @@ func main() {
 	if part == 1 {
 		result = part1(data, realData)
 	} else {
-		result = part2(data)
+		result = part2(data, realData)
 	}
 
 	fmt.Println(result)
@@ -55,6 +56,54 @@ func check(number int, numbers []int) bool {
 	return false
 }
 
-func part2(data []string) int {
-	return 0
+func part2(data []string, realData bool) int {
+	numbers := make([]int, len(data))
+	for i, line := range data {
+		numbers[i], _ = strconv.Atoi(line)
+	}
+
+	invalidNumber := part1(data, realData)
+
+	currentSum := 0
+
+	currentLow := 0
+	currentHigh := 0
+
+	for i, n := range numbers {
+		if currentSum == invalidNumber && currentLow != currentHigh {
+			break
+		}
+
+		if currentSum < invalidNumber {
+			currentHigh = i
+			currentSum += n
+		}
+
+		if currentSum > invalidNumber {
+			for j := currentLow; j < currentHigh; j++ {
+				if currentSum <= invalidNumber {
+					break
+				}
+
+				currentSum -= numbers[j]
+				currentLow = j + 1
+			}
+		}
+	}
+
+	fmt.Println(currentLow, currentHigh)
+
+	minimum := math.MaxInt
+	maximum := 0
+
+	for _, n := range numbers[currentLow : currentHigh+1] {
+		if n < minimum {
+			minimum = n
+		}
+		if n > maximum {
+			maximum = n
+		}
+	}
+
+	return minimum + maximum
 }
