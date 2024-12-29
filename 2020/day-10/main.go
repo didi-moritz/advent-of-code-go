b88753bab581771106ea0c5f645fbb0a7bc68212
Dietmar Moritz <didi.moritz@gmail.com>
2024-12-29 19:29:54 +0100

    add day-10-2 (2020)

--- 2020/day-10/main.go
+++ 2020/day-10/main.go
@@ -8,21 +8,10 @@ import (
 )
 
 func main() {
-	part, realData := utils.GetRunConfig(1, false)
+	part, realData := utils.GetRunConfig(2, false)
 
 	data := utils.ReadFileAsStringArray(utils.GetFileName(2020, 10, realData))
 
-	var result int
-	if part == 1 {
-		result = part1(data)
-	} else {
-		result = part2(data)
-	}
-
-	fmt.Println(result)
-}
-
-func part1(data []string) int {
 	numbers := make([]int, len(data))
 	for i, line := range data {
 		numbers[i], _ = strconv.Atoi(line)
@@ -30,6 +19,17 @@ func part1(data []string) int {
 
 	sort.Ints(numbers)
 
+	var result int
+	if part == 1 {
+		result = part1(numbers)
+	} else {
+		result = part2(numbers)
+	}
+
+	fmt.Println(result)
+}
+
+func part1(numbers []int) int {
 	oneDiffCounts := 0
 	threeDiffCounts := 0
 
@@ -54,6 +54,36 @@ func part1(data []string) int {
 	return oneDiffCounts * threeDiffCounts
 }
 
-func part2(data []string) int {
-	return 0
+func part2(numbers []int) int {
+	allNumbers := make([]int, len(numbers)+1)
+	allNumbers[0] = 0
+	for i, number := range numbers {
+		allNumbers[i+1] = number
+	}
+
+	cache := make(map[int]int)
+
+	return findPermutations(allNumbers, 0, cache)
+}
+
+func findPermutations(numbers []int, i int, cache map[int]int) int {
+	result := 0
+
+	if i == len(numbers)-1 {
+		return 1
+	}
+
+	cachedResult, found := cache[i]
+	if found {
+		return cachedResult
+	}
+
+	maxNextNumber := numbers[i] + 3
+	for j := i + 1; j < len(numbers) && numbers[j] <= maxNextNumber; j++ {
+		result += findPermutations(numbers, j, cache)
+	}
+
+	cache[i] = result
+
+	return result
 }
