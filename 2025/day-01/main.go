5911beb85e48f6eef8a2a73512ed679aacf771a4
Dietmar Moritz <didi.moritz@gmail.com>
2025-12-01 17:55:50 +0100

    add day-01-2 (2025)

--- 2025/day-01/main.go
+++ 2025/day-01/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	part, realData := utils.GetRunConfig(1, false)
+	part, realData := utils.GetRunConfig(2, false)
 
 	data := utils.ReadFileAsStringArray(utils.GetFileName(2025, 1, realData))
 
@@ -52,5 +52,48 @@ func part1(data []string) int {
 }
 
 func part2(data []string) int {
-	return 0
+	pos := 50
+	var newPos int
+
+	result := 0
+
+	for _, line := range data {
+		var command string
+		var steps int
+		fmt.Sscanf(line, "%1s%d", &command, &steps)
+
+		if command == "R" {
+			newPos = pos + steps
+		} else {
+			newPos = pos - steps
+		}
+
+		if newPos >= 100 {
+			result += ((newPos - 100) / 100) + 1
+		} else if newPos <= 0 {
+			result += -(newPos / 100) + 1
+
+			if pos == 0 {
+				result--
+			}
+		}
+
+		pos = newPos
+
+		for pos < 0 {
+			pos += 100
+		}
+
+		for pos > 99 {
+			pos -= 100
+		}
+
+		//if pos == 0 {
+		//	result++
+		//}
+
+		fmt.Println(line, pos, result)
+	}
+
+	return result
 }
